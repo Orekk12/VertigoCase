c4954bc597e8cf3dc9248a595b5674f20c2fd931
keremkz <kurnazkerem@sabanciuniv.edu>
2023-03-30 20:50:03 +0300

    add slot scriptable object scripts and objects

---
+++ Assets/ScriptableObjects/Scripts/AllWheelSlotCards.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SlotCardHolder")]
+public class AllWheelSlotCards : ScriptableObject
+{
+    [SerializeField] private WheelSlotCard card1;
+    [SerializeField] private WheelSlotCard card2;
+    [SerializeField] private WheelSlotCard card3;
+    [SerializeField] private WheelSlotCard card4;
+    [SerializeField] private WheelSlotCard card5;
+    [SerializeField] private WheelSlotCard card6;
+    [SerializeField] private WheelSlotCard card7;
+    [SerializeField] private WheelSlotCard card8;
+}
