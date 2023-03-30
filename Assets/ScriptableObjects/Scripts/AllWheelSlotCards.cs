8d7216a9968aa565c12e52a15b11b4d413d0daae
keremkz <kurnazkerem@sabanciuniv.edu>
2023-03-30 21:43:37 +0300

    update all slot card scriptable object to use a list

--- Assets/ScriptableObjects/Scripts/AllWheelSlotCards.cs
+++ Assets/ScriptableObjects/Scripts/AllWheelSlotCards.cs
@@ -5,12 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "SlotCardHolder")]
 public class AllWheelSlotCards : ScriptableObject
 {
-    [SerializeField] private WheelSlotCard card1;
-    [SerializeField] private WheelSlotCard card2;
-    [SerializeField] private WheelSlotCard card3;
-    [SerializeField] private WheelSlotCard card4;
-    [SerializeField] private WheelSlotCard card5;
-    [SerializeField] private WheelSlotCard card6;
-    [SerializeField] private WheelSlotCard card7;
-    [SerializeField] private WheelSlotCard card8;
+    [SerializeField] private List<WheelSlotCard> cardList;
+    public List<WheelSlotCard> CardList => cardList;
 }
