f115b135680966221d21e633b95f92340b40e72e
Kerem Kurnaz <kurnazkerem@sabanciuniv.edu>
2023-03-31 16:44:30 +0300

    add spinbutton prefab and script

---
+++ Assets/Scripts/SpinButton.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpinButton : MonoBehaviour
+{
+    private WheelRotation wheelRotation;
+    private Button spinButton;
+
+    private void Awake()
+    {
+        spinButton = GetComponent<Button>();
+        wheelRotation = GameObjectManager.Instance.WheelRotation;
+
+        if (spinButton && wheelRotation)
+        {
+            spinButton.onClick.AddListener(()=> wheelRotation.StartRotation());
+        }
+    }
+}
